36eccc1823263eade45fe8935cf8038bf211da63
morzepanke <99017438+morzepanke@users.noreply.github.com>
2022-02-04 13:22:14 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="rs">
+<head>
+<!-- Global site tag (gtag.js) - Google Ads: 618057255 -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-618057255"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-618057255');
+</script>
+
+
+
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
+    <title>DODATNA NEGA ZA MERE I ODRŽAVANJE!</title>
+    <meta content="width=380" name="viewport">
+    <meta content="width" name="MobileOptimzied">
+    <link href="favicon.ico" rel="shortcut icon" type="image/x-icon">
+    <link rel="stylesheet" href="./index_files/form.css">
+    <link href="./index_files/index.css" media="all" rel="stylesheet" type="text/css">
+
+    <link href="./index_files/jsdguifgwsiugiqdgqweifgdqwdqfd.css" type="text/css" rel="stylesheet">
+    <script src="./index_files/jquery-2.2.4.min.js"></script>
+    <style type="text/css">
+      .lucky-chat-message__name-avatar {
+        width:33px !important;
+        height:33px !important;
+        background:url('2233.png') !important;
+      }
+    </style>
+<script type="text/javascript" src="script_land.js" defer></script></head>
+
+<body>
+
+
+                                        <div align="center">
+                                            <center>
+                                                <div class="coolform-4" id="form">
+                                                    <h2>Pažnja!</h2>
+                                                    <p class="date-sale">Državni program
+                                                        <span class="action">
+                                                            <script>dtime_nums(-1, true);</script>                                                         </span></p>
+                                                    <p class="date-sale-bottom">
+                                                    Potpuni obrazac i dobićete stavku sa popustom! <b> Količina artikla sa popustom je ograničena!</b>
+                                                    </p>
+                                                    <section class="myContent">
+                                                        <div class="form-wrap">
+                                                            <p class="action-valid"> Obrazac za registraciju</p>
+                                                            <div class="price-block">
+                                                                <p class="price-content">CENA <br><span>по акцији</span>
+                                                                </p>
+                                                                <p class="price-numb"><span class="new_price_val al-raw-cost-promo">3700</span> <span class="new_price_cur al-raw-currency">din</span></p>
+                                                            </div>
+                                                            <p class="lp-text"><span class="left-pack js-countdown">11</span>
+                                                                pakovanje postavljeno na lageru!</p>
+                                                            <form method="post" action="/land/order" class="al-form">
+                                                                <select class="al-country" name="country" style="display: none;"></select>
+                                                                <div><input class="name-black" name="name" placeholder="Име" required="" type="text" value=""></div>
+                                                                <div><input class="phone-black" name="phone" placeholder="Телефон" required="" type="tel" value=""></div>
+                                                                <input class="event_order" type="submit" value="NARUČITE PARAZOL!">
+                                                                <small>* cena po porcijama</small>
+                                                                <br>
+                                                            </form>
+                                                        </div>
+                                                    </section>
+
+                                                   
+                                                </div>
+                                            </center>
+                                        </div>
+                                    
+
+
+
+
+<script src="./index_files/comments.min.js"></script>
+<script src="./index_files/countdown.js"></script>
+<script src="./index_files/tl-validator.js?country=rsb&label=false"></script>
+<style>.product_item__title i{margin-right:3px;}</style>
+</body>
+</html>
